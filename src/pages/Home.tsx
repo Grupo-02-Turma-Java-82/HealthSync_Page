86bd24e6526c34fcc685eec202bfb72a19c978ae
Lívia D'Alexandri Cunha <liviadlxcunha@gmail.com>
2025-07-10 19:15:20 -0300

    Implementação da Hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
-import { Features } from "@/components/Features";
+import { Feedbacks } from "@/components/Feedbacks";
+import { Hero } from "@/components/Hero";
 
 export function Home() {
-  return <Features></Features>;
+  return (
+    <div>
+      <Hero />
+      <Feedbacks />
+    </div>
+  );
 }
