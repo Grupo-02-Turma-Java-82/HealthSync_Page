f825aa2af0b19a12edae3d9a856620e53fb43e7e
Helena <maria.helen@live.com>
2025-07-10 19:54:47 -0300

    :sparkles: Refactor UserLayout to include SidebarProvider and AppSidebar component

--- src/components/UserLayout.tsx
+++ src/components/UserLayout.tsx
@@ -1,11 +1,23 @@
 import { Outlet } from "react-router";
 import { NavbarDashboard } from "./NavbarDashboard";
+import { SidebarProvider } from "./ui/sidebar";
+import { AppSidebar } from "./app-sidebar";
 
 export function UserLayout() {
   return (
     <main className="w-full md:w-auto">
-      <NavbarDashboard />
-      <Outlet />
+      <SidebarProvider>
+        <AppSidebar />
+
+        <main className="w-screen h-full">
+          <NavbarDashboard
+            nameUser="Ricardo"
+            typeUser="Aluno"
+            photo="https://ik.imagekit.io/brunogodoy/default-image.jpg?updatedAt=1747847809671"
+          />
+          <Outlet />
+        </main>
+      </SidebarProvider>
     </main>
   );
 }
